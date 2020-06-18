43af5f64c358f28300348ee48b35600c48fa366a
Heidi Sprouse <hsprouse91@gmail.com>
2020-06-18 14:34:51 -0500

    Added a utility class to convert seconds to minute and hour string. Formatted time now shows on walker details page

---
+++ DogGo/Utilities/TimeUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Utilities
+{
+    public class TimeUtils
+    {
+        //converts seconds into a string with the hours and minutes
+        public static string SecondsToHoursAndMinutes(int seconds)
+        {
+            int hours = (seconds % (24 * 3600)) / 3600;
+            int minutes = (seconds % (24 * 3600 * 3600)) / 60;
+
+            return $"{(hours < 1 ? "" : $"{ hours}hr ")}{minutes}min";
+        }
+    }
+}
