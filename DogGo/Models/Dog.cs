7672ba42053d14506038bc5abca820142ebca0fd
Heidi Sprouse <hsprouse91@gmail.com>
2020-06-22 09:39:11 -0500

    Dogs and owners now have form field validation

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace DogGo.Models
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Your dog needs a name!")]
+        [MaxLength(35)]
         public string Name { get; set; }
         public int OwnerId { get; set; }
+        [Required]
+        [MaxLength(35)]
         public string Breed { get; set; }
         public string Notes { get; set; }
         public string ImageUrl { get; set; }
